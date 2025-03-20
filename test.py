28247db557f6b335b65e28365b5f6ce3361d9b45
ch Nitheesh <chalumuri.nitheesh@gmail.com>
2025-03-21 01:06:32 +0530

    adding everythin till website done

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from sklearn.neighbors import KNeighborsClassifier
-
-
 import pickle
 import cv2 as cv
 import numpy as np
 import os
+import csv
+import time
+from datetime import datetime
 video = cv.VideoCapture(0)
 facedetect = cv.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
@@ -15,6 +16,8 @@ with open("data/faces_data.pkl",'rb') as f:
 
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(FACES,LABLES)
+COL_NAMES = ['NAME','TIME']
+
 
 while True:
     ret,frame = video.read()
@@ -25,12 +28,35 @@ while True:
         crop_img = frame[y:y+h,x:x+w, : ]
         resized_img = cv.resize(crop_img,(50,50)).flatten().reshape(1,-1)
         output = knn.predict(resized_img)
+        
+        ts = time.time()
+        date = datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
+        timestamp = datetime.fromtimestamp(ts).strftime("%H:%M-%S")
+        exist = os.path.isfile("Attendence/Attendence_" + date + ".csv")
+        cv.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),1)
+        cv.rectangle(frame,(x,y),(x+w,y+h),(50,50,255),2)
+        cv.rectangle(frame,(x,y-40),(x+w,y),(50,50,255),-1)
+
         cv.putText(frame,str(output[0]),(x,y-15),cv.FONT_HERSHEY_COMPLEX,1,(255,255,255),1)
         cv.rectangle(frame,(x,y),(x + w,y + h),(50,50,255),1)
-
+        attendence=[str(output[0]),str(timestamp)]
     cv.imshow("frame",frame)
     k = cv.waitKey(1)
+    if k == ord("o"):
+        if exist:
+            with open("Attendence/Attendence_" + date + ".csv","+a") as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(attendence)
+            csvfile.close()
+        else:
+            with open("Attendence/Attendence_" + date + ".csv","+a") as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(COL_NAMES)
+                writer.writerow(attendence)
+            csvfile.close()
     if k == ord('q'):
         break
 video.release()
 cv.destroyAllWindows()
+
+
